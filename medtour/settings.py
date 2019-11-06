edf28becbe9f2fe3145aa51a48331fe9c80891a3
Aashutosh Rathi <aashutoshrathi@gmail.com>
2019-11-06 12:51:23 +0530

    Add static path

--- medtour/settings.py
+++ medtour/settings.py
@@ -174,6 +174,7 @@ LOGOUT_REDIRECT_URL = 'home'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "medtour/static"),
